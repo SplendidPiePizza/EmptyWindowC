626bec632c09957c8c726bd1048e081fba5a64ed
SplendidPiePizza <165284283+SplendidPiePizza@users.noreply.github.com>
2024-11-16 21:26:32 +0800

    First File Commit

---
+++ settings.h
@@ -0,0 +1,12 @@
+// settings.h
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+// Window settings
+#define TITLE ""               // Empty window title
+#define BACKGROUND_COLOR RGB(255, 255, 255)  // White background color
+#define WIDTH 800              // Width of the window
+#define HEIGHT 600             // Height of the window
+
+#endif // SETTINGS_H
